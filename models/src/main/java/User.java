877e11eb3075168dcb043b61da594afdf2e3dbc3
Mert Gonkesen <mert@yourporter.com>
2022-09-25 13:26:10 +0200

    model module

--- models/src/main/java/User.java
+++ models/src/main/java/User.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class User {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+    private transient String password;
+
 }
